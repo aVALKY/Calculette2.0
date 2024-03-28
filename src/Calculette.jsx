fbaa2f960fff7a8676597eae9bdd85fcb896a26b
aVALKY <Fillierelouis1@gmail.com>
2024-03-28 15:45:05 +0100

    ajout du jsx sur la calculatrice avec la possibilité d'utiliser les additions et les egal

--- src/Calculette.jsx
+++ src/Calculette.jsx
@@ -1,44 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css';
 
+
 const Calculette = () => {
+const [total, setTotal] = useState("");
+
+
+const caracterToAdd = (e) => {
+    console.log(e.target.innerHTML)
+    setTotal(total+e.target.innerHTML)
+}
+
+const calc = () => {
+    if (total !== ""){
+        setTotal(eval(total));
+    }
+}
+
+
+
     return <>
-    <link rel="stylesheet" href="style.css" />
+
+        <link rel="stylesheet" href="style.css" />
         <h1> Calculatrice 2.0 </h1>
-    <div class="calculator card">
+        <div className="calculator card">
 
-  <input type="text" class="calculator-screen z-depth-1" value="" disabled />
+            <input type="text" className="calculator-screen z-depth-1" value={total} disabled />
 
-  <div class="calculator-keys">
+            <div className="calculator-keys">
 
-    <button type="button" class="operator btn btn-info" value="+">+</button>
-    <button type="button" class="operator btn btn-info" value="-">-</button>
-    <button type="button" class="operator btn btn-info" value="*">&times;</button>
-    <button type="button" class="operator btn btn-info" value="/">&divide;</button>
+                <button type="button" className="operator btn btn-info " onClick={caracterToAdd} value="+">+</button>
+                <button type="button" className="operator btn btn-info " onClick={caracterToAdd} value="-">-</button>
+                <button type="button" className="operator btn btn-info " onClick={caracterToAdd} value="*">&times;</button>
+                <button type="button" className="operator btn btn-info " onClick={caracterToAdd} value="/">&divide;</button>
 
-    <button type="button" value="7" class="btn btn-light waves-effect">7</button>
-    <button type="button" value="8" class="btn btn-light waves-effect">8</button>
-    <button type="button" value="9" class="btn btn-light waves-effect">9</button>
-
-
-    <button type="button" value="4" class="btn btn-light waves-effect">4</button>
-    <button type="button" value="5" class="btn btn-light waves-effect">5</button>
-    <button type="button" value="6" class="btn btn-light waves-effect">6</button>
+                <button type="button" value="7" className="btn waves-effect chiffre " onClick={caracterToAdd}>7</button>
+                <button type="button" value="8" className="btn waves-effect chiffre " onClick={caracterToAdd}>8</button>
+                <button type="button" value="9" className="btn waves-effect chiffre " onClick={caracterToAdd}>9</button>
 
 
-    <button type="button" value="1" class="btn btn-light waves-effect">1</button>
-    <button type="button" value="2" class="btn btn-light waves-effect">2</button>
-    <button type="button" value="3" class="btn btn-light waves-effect">3</button>
+                <button type="button" value="4" className="btn waves-effect chiffre " onClick={caracterToAdd}>4</button>
+                <button type="button" value="5" className="btn waves-effect chiffre " onClick={caracterToAdd}>5</button>
+                <button type="button" value="6" className="btn waves-effect chiffre " onClick={caracterToAdd}>6</button>
 
 
-    <button type="button" value="0" class="btn btn-light waves-effect">0</button>
-    <button type="button" class="decimal function btn btn-secondary" value=".">.</button>
-    <button type="button" class="all-clear function btn btn-danger btn-sm" value="all-clear">AC</button>
+                <button type="button" value="1" className="btn waves-effect chiffre " onClick={caracterToAdd}>1</button>
+                <button type="button" value="2" className="btn waves-effect chiffre " onClick={caracterToAdd}>2</button>
+                <button type="button" value="3" className="btn waves-effect chiffre " onClick={caracterToAdd}>3</button>
 
-    <button type="button" class="equal-sign operator btn btn-default" value="=">=</button>
 
-  </div>
-</div>
+                <button type="button" value="0" className="btn waves-effect " onClick={caracterToAdd}>0</button>
+                <button type="button" className="decimal function btn btn-secondary" onClick={caracterToAdd}  value=".">.</button>
+                <button type="button" className="all-clear function btn btn-danger btn-sm" value="all-clear" >AC</button>
+
+                <button  type="button" className="equal-sign operator btn btn-default" value="=" onClick={calc} >=</button>
+            </div>
+        </div>
     </>
 }
 
